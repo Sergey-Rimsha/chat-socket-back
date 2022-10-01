ca5bf37942780f13bbec4b6ceb36482b73037859
Sergey <Sergey.rimsha.ru@gmail.com>
2022-10-01 15:59:40 +0300

    add endpoint socket io new massage and user name

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,51 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const app = express();
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+const PORT = process.env.PORT || 7001;
+const state = [
+    { message: 'hello', id: '4545', user: { id: '45r4as5', name: 'Sergey' } },
+    { message: 'hello', id: '454ds5', user: { id: '45e4as5', name: 'Sasha' } },
+    { message: 'hello', id: '4asd545', user: { id: '45s4as5', name: 'Sveta' } },
+];
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello world get endpoint');
+    console.log('user connected / app');
 });
-app.listen(7001, () => {
-    console.log('The application is listening on port 7001!');
+const usersState = new Map();
+io.on("connection", (socket) => {
+    usersState.set(socket, { id: new Date().getDate().toString(), name: 'anonym' });
+    socket.on('client-name-set', (name) => {
+        if (typeof name !== 'string') {
+            return;
+        }
+        const user = usersState.get(socket);
+        user.name = name;
+    });
+    socket.on('client-message-sent', (message) => {
+        if (typeof message !== 'string') {
+            return;
+        }
+        const user = usersState.get(socket);
+        const id = Math.random();
+        const newMessage = { message, id: new Date().getTime().toString(), user: { id: user.id, name: user.name } };
+        state.push(newMessage);
+        socket.emit('new-message-sent', newMessage);
+    });
+    socket.emit('init-messages-published', state);
+    console.log('user connected socket io');
 });
+httpServer.listen(PORT);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,61 @@
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import {createServer} from "http";
+import {Server} from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {
+	cors: {
+		methods: ["GET", "POST"],
+		allowedHeaders: ["my-custom-header"],
+		credentials: true
+	}
+});
+
+const PORT = process.env.PORT || 7001;
+
+const state = [
+	{ message: 'hello', id: '4545', user: { id: '45r4as5', name: 'Sergey' } },
+	{ message: 'hello', id: '454ds5', user: { id: '45e4as5', name: 'Sasha' } },
+	{ message: 'hello', id: '4asd545', user: { id: '45s4as5', name: 'Sveta' } },
+]
 
 app.get('/', (req, res) => {
 	res.send('Hello world get endpoint')
+	console.log('user connected / app');
 })
 
+const usersState = new Map();
+
 io.on("connection", (socket) => {
-	// ...
-	console.log('user connected');
+
+	usersState.set(socket, { id: new Date().getDate().toString(), name: 'anonym' })
+
+	socket.on('client-name-set', (name: string) => {
+		if (typeof name !== 'string') {
+			return
+		}
+		const user = usersState.get(socket)
+		user.name = name;
+	})
+
+	socket.on('client-message-sent', (message: string) => {
+		if (typeof message !== 'string') {
+			return
+
+		}
+		const user = usersState.get(socket)
+
+		const id = Math.random()
+		const newMessage = { message, id: new Date().getTime().toString(), user: { id: user.id, name: user.name } };
+		state.push(newMessage)
+
+		socket.emit('new-message-sent', newMessage);
+	})
+
+	socket.emit('init-messages-published', state)
+
+	console.log('user connected socket io');
 });
 
-httpServer.listen(7001);
+httpServer.listen(PORT);
